6f962e347010dfee939d799b6724879c1d6237fc
Arguish <jhergon8@gmail.com>
2023-03-28 14:08:29 +0100

    controller media ready

--- db/index.db.js
+++ db/index.db.js
@@ -1,35 +1,42 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require("sequelize");
 
-
-const conn = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-	host: process.env.HOST,
-	dialect: 'mysql',
-	port: process.env.DB_PORT,
-	// logging: true,
-})
-
+const conn = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+    // logging: true,
+  }
+);
 
 async function checkConnection() {
-	try {
-		await conn.authenticate()
-		console.log('Connection to DB has been established successfully.')
-	} catch (error) {
-		throw error
-	}
+  try {
+    await conn.authenticate();
+    console.log("Connection to DB has been established successfully.");
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function syncModels(value) {
-	const state = {
-		alter: { alter: true },
-		force: { force: true },
-	}
+  const state = {
+    alter: { alter: true },
+    force: { force: true },
+  };
 
-	try {
-		await conn.sync(state[value] || '')
-		console.log(`All models were synchronized successfully using sync(${JSON.stringify(state[value]) || ''}).`)
-	} catch (error) {
-		throw error
-	}
+  try {
+    await conn.sync(state[value] || "");
+    console.log(
+      `All models were synchronized successfully using sync(${
+        JSON.stringify(state[value]) || ""
+      }).`
+    );
+  } catch (error) {
+    throw error;
+  }
 }
 
-module.exports = {checkConnection, syncModels}
+module.exports = { checkConnection, syncModels, conn };
--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const api = express()
-const morgan = require('morgan')
-const env = process.env
-const {checkConnection,syncModels} = require('./db/index.db.js')
-const addRelationsToModels = require('./db/relation.db.js')
-
-
+const express = require("express");
+const api = express();
+const morgan = require("morgan");
+const env = process.env;
+const { checkConnection, syncModels } = require("./db/index.db.js");
+const addRelationsToModels = require("./db/relation.db.js");
 
 function initializeAndListenWithExpress() {
-	const api = express()
-		.use(morgan('dev'))
-		.use(express.json())
-		//.use('/api', require('./api/routes'))
+  const api = express()
+    .use(morgan("dev"))
+    .use(express.json())
+    .use("/api", require("./api/routes/index.routes"))
 
-		.listen(env.PORT, () => {
-			console.log(`> Listening on port: ${env.PORT}`)
-		})
+    .listen(env.PORT, () => {
+      console.log(`> Listening on port: ${env.PORT}`);
+    });
 }
 
 async function startAPI() {
-	await initializeAndListenWithExpress()
-	await syncModels('force')
-	await addRelationsToModels()
+  await initializeAndListenWithExpress();
+  await syncModels("force");
+  await addRelationsToModels();
 }
 
-startAPI()
+startAPI();
