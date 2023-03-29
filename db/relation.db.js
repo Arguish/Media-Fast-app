0423339774ee8eb3f56fa63defe99fb7cd7f1319
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-03-29 20:36:53 +0100

    relation media-platform

--- db/relation.db.js
+++ db/relation.db.js
@@ -1,10 +1,9 @@
 const Category = require("../api/models/category.model");
 const PrivateInfo = require("../api/models/private_info.model");
 const Media = require("../api/models/media.model");
-const Platform = require('../api/models/platform.model')
-const UserMedia = require('../api/models/user_media.model')
-const User = require('../api/models/user.model')
-
+const Platform = require("../api/models/platform.model");
+const UserMedia = require("../api/models/user_media.model");
+const User = require("../api/models/user.model");
 
 function addRelationsToModels() {
   try {
@@ -14,13 +13,14 @@ function addRelationsToModels() {
     User.belongsToMany(Media, { as: "user_id", through: "user_media" });
     Media.belongsToMany(User, { as: "media_id", through: "user_media" });
 
-
-    User.belongsToMany(Media, { through: UserMedia })
-    Media.belongsToMany(User, { through: UserMedia })
-
+    User.belongsToMany(Media, { through: UserMedia });
+    Media.belongsToMany(User, { through: UserMedia });
 
     Media.belongsToMany(Category, { through: "media_category" });
     Category.belongsToMany(Media, { through: "media_category" });
+
+    Media.belongsToMany(Platform, { through: "media_platform" });
+    Platform.belongsToMany(Media, { through: "media_platform" });
 
     User.belongsToMany(Category, { through: "user_category" });
     Category.belongsToMany(User, { through: "user_category" });
