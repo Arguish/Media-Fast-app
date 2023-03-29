3178e1b9f87fc630e308bfd05f13feb64422121e
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-03-29 21:15:06 +0100

    GO GO USER ADMIN

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,20 +1,33 @@
-const { getUsers, getOneUser, updateUser, createUser, deleteUser, getOwnUser, getOwnUserMedia, addMediaToOwnUser, getUserMedia, updateOwnUser, deleteOwnUser } = require('../controllers/user.controller')
-const { checkAdmin, checkAuth } = require('../middleware/auth')
+const {
+  getUsers,
+  getOneUser,
+  updateUser,
+  createUser,
+  deleteUser,
+  getOwnUser,
+  getOwnUserMedia,
+  addMediaToOwnUser,
+  getUserMedia,
+  updateOwnUser,
+  deleteOwnUser,
+} = require("../controllers/user.controller");
+const { checkAdmin, checkAuth } = require("../middleware/auth");
+const { goAdmin, goUser } = require("../middleware/authCopy");
 
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', checkAdmin, getUsers)
-router.get('/me', checkAuth, getOwnUser)
-router.get('/me/user_media', checkAuth, getOwnUserMedia)
+router.get("/", goAdmin, getUsers);
+router.get("/me", goUser, getOwnUser);
+router.get("/me/user_media", checkAuth, getOwnUserMedia);
 
-router.get('/:userId', checkAuth, checkAdmin, getOneUser)
-router.get('/:userId/user_media', checkAuth, checkAdmin, getUserMedia)
+router.get("/:userId", checkAuth, checkAdmin, getOneUser);
+router.get("/:userId/user_media", checkAuth, checkAdmin, getUserMedia);
 
-router.put('/me', checkAuth, updateOwnUser)
-router.put('/:userId', checkAuth, checkAdmin, updateUser)
-router.post('/', checkAuth, checkAdmin, createUser)
-router.post('/me/user_media/:mediaId', checkAuth, addMediaToOwnUser)
-router.delete('/me', checkAuth, deleteOwnUser)
-router.delete('/:userId', checkAuth, checkAdmin, deleteUser)
+router.put("/me", checkAuth, updateOwnUser);
+router.put("/:userId", checkAuth, checkAdmin, updateUser);
+router.post("/", checkAuth, checkAdmin, createUser);
+router.post("/me/user_media/:mediaId", checkAuth, addMediaToOwnUser);
+router.delete("/me", checkAuth, deleteOwnUser);
+router.delete("/:userId", checkAuth, checkAdmin, deleteUser);
 
-module.exports = router
+module.exports = router;
