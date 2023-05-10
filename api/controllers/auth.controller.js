d956ced2dab66f91d3cc47c58ddf773a853833fc
Airammg <airam.marting@gmail.com>
2023-05-10 15:37:09 +0100

    Updated auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,14 +9,22 @@ const register = async (req, res) => {
     req.body.password = bcrypt.hashSync(req.body.password, 10)
     const url = 'https://www.robohash.org/' + req.body.nickname
     req.body.img_url = url
-    const user = await User.create(req.body)
-    const privateinfo = await PrivateInfo.create(req.body)
-    const result = await privateinfo.setUser(user)
-    const token = jwt.sign({ email: privateinfo.email }, process.env.SECRET)
-    res.status(200).json({user, privateinfo, token})
+    const userList = await User.findAll({
+      include: [PrivateInfo]
+    })
+    const emails = userList.map((el) => el.dataValues.private_info.dataValues.email)
+    const nicknames = userList.map((el) => el.dataValues.nickname)
+    if (nicknames.includes(req.body.nickname) || emails.includes(req.body.email)) {
+      return res.status(501).send('DATA ALREADY USED.')
+    } else {
+      const user = await User.create(req.body)
+      const privateinfo = await PrivateInfo.create(req.body)
+      const result = await privateinfo.setUser(user)
+      const token = jwt.sign({ email: privateinfo.email }, process.env.SECRET)
+      return res.status(200).json({ user, privateinfo, token })
+    }
   } catch (error) {
-    console.error(error)
-    res.status(500).send('Error cannot create user')
+    return res.status(500).send('Error cannot create user', error)
   }
 }
 
