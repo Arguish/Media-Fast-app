bd6b87171171fd3427b8b88338dacedcdbc8976a
Airammg <airam.marting@gmail.com>
2024-01-06 00:14:15 +0000

    MediaFastBack 🗃️  returned to previous DB connection

--- db/index.db.js
+++ db/index.db.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require("sequelize");
 
-const conn = new Sequelize(`${process.env.DIALECT}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+// const conn = new Sequelize(`${process.env.DIALECT}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
 
-// const conn = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASS, {
-//   host: process.env.HOST,
-//   dialect: process.env.DIALECT,
-//   logging: false
-// })
+const conn = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASS, {
+  host: process.env.HOST,
+  dialect: process.env.DIALECT,
+  logging: false
+})
 
 
 async function checkConnection() {
